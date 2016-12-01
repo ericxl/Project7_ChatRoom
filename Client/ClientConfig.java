13d787fd850270a2053297b3f7b1acbf333364dd
Eric Liang <jklelxy@gmail.com>
2016-11-30 18:46:33 -0600

    removed command line (#26)

--- Client/ClientConfig.java
+++ Client/ClientConfig.java
@@ -4,6 +4,6 @@ package assignment7.Client;
  * Created by Eric on 11/29/16.
  */
 public class ClientConfig {
-    public static final int port = 4242;
+    public static final int port = 9339;
     public static final String address= "localhost";
 }
